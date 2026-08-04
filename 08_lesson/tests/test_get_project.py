53268bcb3454d67f261c8cbec6e3f70adbe50d73
Krylovasveta <linainarina@gmail.com>
2026-08-04 21:40:19 +0300

    2

--- 08_lesson/tests/test_get_project.py
+++ 08_lesson/tests/test_get_project.py
@@ -1,23 +1,25 @@
-
 from pages.projectapi import ProjctApi
+
 
 def test_get_positive():
 
     api = ProjctApi("https://ru.yougile.com/api-v2")
-    t = api.get_project("ba351ac5-f551-4e0f-aea3-81c9336ad780")    
+    t = api.get_project("ba351ac5-f551-4e0f-aea3-81c9336ad780")
 
     assert t[1] == 200, "Ожидался код 200, но пришло {t[1]}"
+
 
 def test_get_empty():
 
     api = ProjctApi("https://ru.yougile.com/api-v2")
-    t = api.get_project("")    
+    t = api.get_project("")
 
-    assert t[1] == 200, "Ожидался код 200, но пришло {t[1]}"    
+    assert t[1] == 200, "Ожидался код 200, но пришло {t[1]}"
+
 
 def test_get_incorrect():
 
     api = ProjctApi("https://ru.yougile.com/api-v2")
-    t = api.get_project("666")    
+    t = api.get_project("666")
 
     assert t[1] == 200, "Ожидался код 200, но пришло {t[1]}"
